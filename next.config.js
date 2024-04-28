6cd2185d0036ec95c9224d4db47e8a0d6de82ae6
Rickyko <ickyr819@gmail.com>
2024-04-28 14:55:44 +0800

    myblog v.1.0.0

--- next.config.js
+++ next.config.js
@@ -7,6 +7,18 @@ const nextConfig = {
     images: {
         domains: ["res.cloudinary.com"],
     },
+    reactStrictMode: true,
+    swcMinify: true,
+    
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+        config.resolve.fallback = {
+            fs: false,
+        };
+        }
+    
+        return config;
+    },
 };
 
 module.exports = nextConfig;
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,15 +1,16 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent } from '@/components/ui/card';
 import {
   Carousel,
-  CarouselMainContainer,
+  CarouselContent,
+  CarouselItem,
   CarouselNext,
   CarouselPrevious,
-  SliderMainItem,
-  CarouselThumbsContainer,
-  SliderThumbItem,
-} from "@/components/ui/carousel";
+  type CarouselApi,
+} from '@/components/ui/carousel';
 import { v2 as cloudinary } from 'cloudinary';
 import styles from '../about/Page.module.css';
-
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
@@ -29,55 +30,60 @@ async function getData({ searchParams }: { searchParams: any }) {
   return resources;
 }
 
-export default async function Page({ searchParams }: { searchParams: any }) {
-  const resources = await getData({ searchParams });
+export default function Page({ searchParams }: { searchParams: any }) {
+  const [resources, setResources] = useState<any[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const fetchData = async () => {
+    const data = await getData({ searchParams });
+    setResources(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [searchParams]);
+
+  const handleNextSlide = () => {
+    setCurrentIndex((prevIndex: number) => (prevIndex === resources.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const handlePreviousSlide = () => {
+    setCurrentIndex((prevIndex: number) => (prevIndex === 0 ? resources.length - 1 : prevIndex - 1));
+  };
+
   return (
     <div className="h-screen flex flex-col justify-center items-center text-center" style={{ transform: 'scale(1.0)', transformOrigin: 'top left' }}>
-  <div id="home-about-content" className="absolute w-full h-full text-center inline-block align-middle leading-normal py-8 px-4 max-w-full text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl" style={{ width: "65ch", fontFamily: 'FATIB'}}>
-    <h2 className={`${styles.HomePageAboutMeTitlefontcolor} text-5xl sm:text-7xl md:text-6xl `}>
-      <span>Test</span>
-    </h2>
-    <div className="flex-grow h-screen flex flex-col justify-center items-center text-center" style={{ position: 'relative', top: '-15vh'  }}>
-      <Carousel className="w-full max-w-md">
-        <CarouselNext className="top-1/3 -translate-y-1/3" />
-        <CarouselPrevious className="top-1/3 -translate-y-1/3" />
-        <CarouselMainContainer className="h-60">
-
-        {Array.from({ length: resources.length }).map((_, index) => (
-          <SliderMainItem key={index} className="bg-transparent">
-            {resources[index] && resources[index].secure_url && resources[index].resource_type === 'image' ? (
-              <img src={resources[index].secure_url} style={{ objectFit: 'cover', width: '100%', height: '100%' }}/>
-            ) : (
-              <video
-              src={resources[index].secure_url}
-              width={resources[index].width}
-              height={resources[index].height}
-              controls style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-            />
-            )}
-          </SliderMainItem>
-        ))}
-
-        </CarouselMainContainer>
-        <CarouselThumbsContainer>
-          {Array.from({ length: resources.length }).map((_, index) => (
-            <SliderThumbItem key={index} index={index} className="bg-transparent">
-              {resources[index] && resources[index].secure_url && resources[index].resource_type === 'image' ? (
-              <img src={resources[index].secure_url} style={{ objectFit: 'cover', width: '100%', height: '100%' }}/>
-            ) : (
-              <video
-              src={resources[index].secure_url}
-              width={resources[index].width}
-              height={resources[index].height}
-              controls style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-            />
-            )}
-            </SliderThumbItem>
-          ))}
-        </CarouselThumbsContainer>
-      </Carousel>
+      <div id="home-about-content" className="absolute w-full h-full text-center inline-block align-middle leading-normal py-8 px-4 max-w-full text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl" style={{ width: "65ch", fontFamily: 'FATIB'}}>
+        <h2 className={`${styles.HomePageAboutMeTitlefontcolor} text-5xl sm:text-7xl md:text-6xl `}>
+          <span>Test</span>
+        </h2>
+        <div className="flex justify-center items-center">
+          <Carousel className="w-full max-w-xs">
+            <CarouselContent>
+              {resources.map((resource, index) => (
+                <CarouselItem key={index}>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        {resource && resource.secure_url ? (
+                          <img src={resource.secure_url} style={{ objectFit: 'cover', width: '100%', height: '100%' }}/>
+                        ) : (
+                          <div>Loading...</div>
+                        )}
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious onClick={handlePreviousSlide} />
+            <CarouselNext onClick={handleNextSlide} />
+          </Carousel>
+        </div>
+        <div className="py-2 text-center text-sm text-muted-foreground">
+          slide {currentIndex + 1} of {resources.length}
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 };
